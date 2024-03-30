18ef9b5ecf37f4e2f5816e39d20b3011e4e492e5
Perceu Bertoletti <perceubertoletti@gmail.com>
2024-03-30 14:06:09 -0300

    adicionado algumas firulas de paralax e tirado evendo de rolagem do mapa, alem de fazer um navstick que se prende no topo ao rolar mantendo a navegação sempre disponivel

---
+++ theme/static/js/scripts.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("navbar");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
